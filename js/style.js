a6aaf87b96ff41c1cb8b66b059066f9d2ab8d0bd
chachamana <info@manamiw.com>
2022-02-05 23:09:12 +0000

    add new files

---
+++ js/style.js
@@ -0,0 +1,115 @@
+//toggle button
+let navToggle = document.querySelector(".nav__toggle");
+let navWrapper = document.querySelector(".nav__wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
+
+
+//Scroll-nav
+   window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("scroll-nav", window.scrollY > 0);
+  });
+
+
+//particle-js
+  particlesJS("particles-js", {
+    "particles":{
+      "number":{
+        "value":346,//この数値を変更すると星の数が増減できる
+        "density":{
+          "enable":true,
+          "value_area":800
+        }
+      },
+      "color":{
+        "value":"#Ffffff"
+      },
+      "shape":{
+        "type":"circle",//形状はcircleを指定
+        "stroke":{
+          "width":0
+        },
+        },
+      "opacity":{
+        "value":1,//シェイプの透明度
+        "random":true,//シェイプの透明度をランダムにする
+        "anim":{
+          "enable":true,//シェイプの透明度をアニメーションさせる
+          "speed":4,//シェイプの透明度をアニメーションさせる
+          "opacity_min":0,//透明度の最小値０
+          "sync":false//全てを同時にアニメーションさせない
+        }
+      },
+      "size":{
+        "value":3,
+        "random":true,
+        "anim":{
+          "enable":false,
+          "speed":4,
+          "size_min":0.3,
+          "sync":false
+        }
+      },
+      "line_linked":{
+        "enable":false,
+      },
+      "move":{
+        "enable":true,
+        "speed":120,//この数値を小さくするとゆっくりな動きになる
+      "direction":"none",//方向指定なし
+      "random":true,//動きはランダムに
+      "straight":true,//動きをとどめる
+        "out_mode":"out",
+        "bounce":false,
+        "attract":{
+          "enable":false,
+          "rotateX":600,
+          "rotateY":600
+        }
+      }
+    },
+    "interactivity":{
+      "detect_on":"canvas",
+      "events":{
+        "onhover":{
+          "enable":false,
+        },
+        "onclick":{
+          "enable":false,
+        },
+        "resize":true
+      }
+    },
+    "retina_detect":true
+  });
+
+
+
+//fade-in
+let fadeInTarget = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+  for (let i = 0; i < fadeInTarget.length; i++){
+    const rect = fadeInTarget[i].getBoundingClientRect().top;
+    const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    const offset = rect + scroll;
+    const windowHeight = window.innerHeight; // 現在のブラウザの高さ
+    if (scroll > offset - windowHeight + 150) {
+      fadeInTarget[i].classList.add('scroll-in');
+    }
+  }
+});
+ScrollReveal({ distance: '100px' });
+ScrollReveal().reveal('.scrollshow-s', { delay: 600,reset: true });
+ScrollReveal().reveal('.scrollshow-m', { delay: 800,reset: true  });
+ScrollReveal().reveal('.scrollshow-bottom', { delay: 600,reset: true });
